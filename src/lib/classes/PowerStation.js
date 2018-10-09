d204be778ae7321066763f6aed767beeaa427268
Ivo Toby <i.toby@wearetriple.com>
2018-10-09 18:20:39 +0200

    remove print methods from class and moved to cliMethods

--- src/lib/classes/PowerStation.js
+++ src/lib/classes/PowerStation.js
@@ -14,27 +14,6 @@ class PowerStation {
         const results = await AuthenticatedGoodWePost('PowerStationMonitor/GetPowerStationPacByDayForApp', params);
         return results;
     }
-
-    static async printPowerstationList() {
-        const stationsList = await PowerStation.getPowerStations();
-        console.log('Found the following powerstations (name, location, powerstation-id):');
-        stationsList.forEach((station) => {
-            console.log(station.stationname, station.location, station.powerstation_id);
-        });
-    }
-
-    static async printLastOutput(stationId) {
-        const today = new Date();
-        const output = await PowerStation.getPowerstationPac(stationId, today);
-        console.log(output);
-        const formattedOutput = {
-            total: output.today_power,
-            // last item always has pac:0.. second last has actual value
-            lastOutput: output.pacs[output.pacs.length - 2].pac,
-            dateLastOutput: output.pacs[output.pacs.length - 2].date,
-        }
-        console.log(formattedOutput);
-    }
-
 }
-module.exports = PowerStation;
+
+export default PowerStation;
